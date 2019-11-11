3ca8d0c0d928ac3019d80890b32c5ff38e941859
caaiomendes <53572543+caaiomendes@users.noreply.github.com>
2019-11-11 09:39:23 -0300

    Add files via upload

---
+++ src/main/java/com/capgemini/batatas/BatatasApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.batatas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatatasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatatasApplication.class, args);
+	}
+
+}
